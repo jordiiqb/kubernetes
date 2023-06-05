9c46624a16597639a0b398e77d232a4817ad75f0
a184311jq <a184311jq@debian-vm>
2023-06-05 10:36:37 +0200

    Creació de nous directoris per separar diferents versions de la app per fer proves amb deployments.

--- arxius/app/app-v2/app-v2.js
+++ arxius/app/app-v2/app-v2.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const os = require('os');
-console.log("Kubia server starting...");
+console.log("App server starting...");
 var handler = function(request, response) {
 console.log("Received request from " + request.connection.remoteAddress);
 response.writeHead(200);
-response.end("You've hit " + os.hostname() + "\n");
+response.end("This is app v2 running. You've hit " + os.hostname() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(8080);
